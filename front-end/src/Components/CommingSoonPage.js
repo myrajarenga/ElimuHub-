2578061e26bd3b5d5e08be41de6fa7abab9a5875
atonya-bravin <bravinatonya.brani@gmail.com>
2024-05-18 12:19:23 +0300

    implementation of the commig soon page

---
+++ front-end/src/Components/CommingSoonPage.js
@@ -0,0 +1,12 @@
+const CommingSoon = () => {
+    return(
+        <div className="w-[100%] h-[100vh] bg-[#2C4035] flex justify-center items-center">
+            <div className="text-[56px] text-[#D2D9D2]">
+                ElimuHub Comming Soon...
+            </div>
+        </div>
+        
+    );
+};
+
+export default CommingSoon;
