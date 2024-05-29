96cfb9a50ac74a5add603af03e547e8e8cdab99d
atonya-bravin <bravinatonya.brani@gmail.com>
2024-05-29 12:02:42 +0300

    Inclusion of the FeaturesTextContainer component

---
+++ front-end/src/Components/LandingPage/Features/FeaturesTextContainer.js
@@ -0,0 +1,30 @@
+/*
+    This module contains the FeaturesTextContainer component.
+    This component contains text related to paticular features.
+*/
+
+//Inclusion of the Link component that helps with navigation.
+import { Link } from "react-router-dom";
+
+const FeaturesTextContainer = ({header, text}) => {
+    return(
+        //Making sure that the container is equsl to the parent in height and text is aligned to the center
+        <div className="px-[32px] flex items-center justify-center h-[100%]">
+            <div className="w-[100%]">
+                <h3 className="ui header">{header}</h3>
+                <div><p>{text}</p></div>
+                <div className="mt-[20px]">
+                    {/* use of the Link component to navigate to /signUp URL */}
+                    <Link to="/signUp">
+                        <button className="ui right labeled icon button red">
+                            <i className="right arrow icon"></i>
+                            Register Today
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FeaturesTextContainer;
