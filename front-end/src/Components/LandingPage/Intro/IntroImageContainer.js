9abfd881493bf5fe3bc2df8a86250fd26b0f15a1
atonya-bravin <bravinatonya.brani@gmail.com>
2024-05-29 11:33:16 +0300

    inclusion of the IntroImageContainer component that contians the image

---
+++ front-end/src/Components/LandingPage/Intro/IntroImageContainer.js
@@ -0,0 +1,17 @@
+/*
+    This module contains the IntroImageContainer component.
+    This component basically contains a centered image to the intro.
+*/
+const IntroImageContainer = () => {
+    return(
+        // Making sure that the image is centered and the width and height of the container are equal to that of the parent.
+        <div className="w-[100%] flex justify-center items-center h-[100%]">
+            {/* giving a padding of 16 pixels to the right to make sure that the image aligns to the sign in button on the nav. */}
+            <div className="pr-[16px]">
+                <img alt="e-learning" src="./Images/IntroImage.jpg" className="w-[100%] rounded-[4px]" />
+            </div>
+        </div>
+    );
+}
+
+export default IntroImageContainer;
