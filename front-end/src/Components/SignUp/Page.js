867e5510a8f1ccb0bef9d35c2ff6344cc9e0971e
atonya-bravin <bravinatonya.brani@gmail.com>
2024-05-29 12:48:00 +0300

    Inclusion of the sign up page with commit Soon message

---
+++ front-end/src/Components/SignUp/Page.js
@@ -0,0 +1,11 @@
+import CommingSoon from "../CommingSoonPage";
+
+const SignUpPage = () => {
+    return(
+        <div className="ui container h-[100vh]">
+            <CommingSoon TimeLine="June 01, 2024 00:00:00"/>
+        </div>
+    );
+};
+
+export default SignUpPage;
