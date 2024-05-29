71fae8738578d3898cf3818c4bcb8c4acc85ea82
atonya-bravin <bravinatonya.brani@gmail.com>
2024-05-29 12:34:04 +0300

    inclusion of the About component

---
+++ front-end/src/Components/LandingPage/Contacts/About.js
@@ -0,0 +1,20 @@
+//This module contains the About component that holds infor about ElimuHub
+
+const About = () => {
+    return(
+        <div>
+            <h4 className="ui header">About ElimuHub</h4>
+            <div className="mt-[20px]">
+            <div class="ui list">
+                <div class="item">About Us</div>
+                <div class="item">ElimuHub Careers</div>
+                <div class="item">Terms and Conditions</div>
+                <div class="item">elimuhub.support.co.ke</div>
+                <div class="item">+254 0111883753</div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
