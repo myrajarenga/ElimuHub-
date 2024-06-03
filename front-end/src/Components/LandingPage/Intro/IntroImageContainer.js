8ac0e983abf0449e739c76206d79a125d5ba5390
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-04 01:06:38 +0300

    Working on the responsiveness of the Intro image container

--- front-end/src/Components/LandingPage/Intro/IntroImageContainer.js
+++ front-end/src/Components/LandingPage/Intro/IntroImageContainer.js
@@ -4,8 +4,11 @@
 */
 const IntroImageContainer = () => {
     return(
-        // Making sure that the image is centered and the width and height of the container are equal to that of the parent.
-        <div className="w-[100%] flex justify-center items-center h-[100%]">
+        /*
+            Making sure that the image is centered and the width and height of the container are equal to that of the parent.
+            Responsive image that hides in small screens
+        */
+        <div className="flex justify-center items-center h-[100%] hidden md:inline-flex">
             {/* giving a padding of 16 pixels to the right to make sure that the image aligns to the sign in button on the nav. */}
             <div className="pr-[16px]">
                 <img alt="e-learning" src="./Images/IntroImage.jpg" className="w-[100%] rounded-[4px]" />
