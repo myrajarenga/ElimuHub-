58c36f4bdcc75cc32658ac26adf9af29e1af2318
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-04 01:44:15 +0300

    Inclusion of the imageURL and improvement of the responsiveness of the container

--- front-end/src/Components/LandingPage/Features/LeftFeaturesContentContainer.js
+++ front-end/src/Components/LandingPage/Features/LeftFeaturesContentContainer.js
@@ -8,22 +8,20 @@ import FeaturesTextContainer from "./FeaturesTextContainer";
 const LeftFeaturesContentContainer = ({imageUrl, header, text, borderDirection}) => {
     return(
         //Creation of the grid and inclusion of a 5px divider
-        <div className="grid grid-cols-2 divide-x-[5px] h-[90vh] w-[100%]">
-            <div className="">
+        //Improvement of the responsivenes using grid difference
+        <div className="grid grid-cols-1 md:grid-cols-2 md:divide-x-[5px] w-[100%]">
                 {/* Making sure that the image is on the left of the gird */}
                 <FeaturesImageContainer 
                     imageUrl={imageUrl}
                     borderDirection={borderDirection}
                 />
-            </div>
-            <div className="">
                 {/* Making sure that the text container is on the right of the gird */}
                 <FeaturesTextContainer
                     header = {header}
                     text = {text}
-                />
-            </div>
-            
+                    //inclusion of the image url to be used by the images that are shown in small screens
+                    imageUrl={imageUrl}
+                />            
         </div>
     );
 };
--- front-end/src/Components/LandingPage/Features/RightFeaturesContentContainer.js
+++ front-end/src/Components/LandingPage/Features/RightFeaturesContentContainer.js
@@ -9,21 +9,20 @@ import FeaturesTextContainer from "./FeaturesTextContainer";
 const RightFeaturedContentContainer = ({imageUrl, header, text, borderDirection}) => {
     return(
         //Creation of the grid and inclusion of a 5px divider
-        <div className="grid grid-cols-2 divide-x-[5px] divide-[#005C53] h-[90vh] w-[100%]">
-            <div className="">
+        //Improvement of the responsivenes using grid difference
+        <div className="grid grid-cols-1 md:grid-cols-2 md:divide-x-[5px] md:divide-[#005C53] w-[100%]">
                 {/* Making sure that the text container is on the left of the gird */}
                 <FeaturesTextContainer
                     header = {header}
                     text = {text}
+                    //inclusion of the image url to be used by the images that are shown in small screens
+                    imageUrl={imageUrl}
                 />
-            </div>
-            <div className="">
                 {/* Making sure that the image is on the right of the gird */}
                 <FeaturesImageContainer 
                     imageUrl={imageUrl}
                     borderDirection={borderDirection}
                 />
-            </div>
             
         </div>
     );
