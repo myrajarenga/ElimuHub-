7226d58167f1f12f40285b1665369fe9d6c6edfa
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-04 01:36:20 +0300

    Working on the FeaturesImageContaier's responsiveness

--- front-end/src/Components/LandingPage/Features/FeaturesImageContainer.js
+++ front-end/src/Components/LandingPage/Features/FeaturesImageContainer.js
@@ -5,8 +5,9 @@
 const FeaturesImageContainer = ({imageUrl, borderDirection}) => {
     return(
         // Making sure that the image is aligned to the center
+        //Making sure that the image hiddes in small screens and shows in large screens. This suplements the responsiveness of the features image container
         <div
-            className= "flex items-center justify-center h-[100%] px-[32px]"
+            className= "md:items-center md:justify-center md:h-[100%] md:px-[32px] hidden md:flex"
         >
             <img
                 className="rounded-[8px]"
