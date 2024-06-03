b18fa0bb13a2764631ce28b92ab72b2d725c3a2f
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-04 01:41:14 +0300

    Inclusion of the image to be shown in large screens and inclusion of space using padding top

--- front-end/src/Components/LandingPage/Features/FeaturesTextContainer.js
+++ front-end/src/Components/LandingPage/Features/FeaturesTextContainer.js
@@ -6,13 +6,22 @@
 //Inclusion of the Link component that helps with navigation.
 import { Link } from "react-router-dom";
 
-const FeaturesTextContainer = ({header, text}) => {
+const FeaturesTextContainer = ({header, text, imageUrl}) => {
     return(
         //Making sure that the container is equsl to the parent in height and text is aligned to the center
-        <div className="px-[32px] flex items-center justify-center h-[100%]">
+        //woking on the responsiveness of the featurestext container using the padding bottom
+        <div className="px-[32px] pb-[40px] md:pb-[40px] flex items-center justify-center h-[100%]">
             <div className="w-[100%]">
                 <h3 className="ui header">{header}</h3>
-                <div><p>{text}</p></div>
+                {/* Inclusion of the image that hides in small screens and shows in large ones */}
+                <img
+                    className="rounded-[8px] md:hidden"
+                    alt="e-learning"
+                    src={imageUrl}
+                />
+
+                {/* Adding of space between the text and the image using padding on the top of the text */}
+                <div className="pt-[20px] md:pt-[0px]"><p>{text}</p></div>
                 <div className="mt-[20px]">
                     {/* use of the Link component to navigate to /signUp URL */}
                     <Link to="/signUp">
