ba861c43808d124523a5cfda68ec41edfc34b515
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-04 01:20:21 +0300

    Inclusion of the Nav container initialy removed from the Landing page module

--- front-end/src/Components/LandingPage/Intro/IntroContainer.js
+++ front-end/src/Components/LandingPage/Intro/IntroContainer.js
@@ -2,11 +2,15 @@
     This module contains code related to the IntroContiner Component.
     This component makes it possible to have a container that carries the intro content.
 */
+import Nav from "../Nav/Nav";
 import IntroContentContainer from "./IntroContentContainer";
 
 const IntroContainer = () => {
     return(
-        <div className="h-[100%] w-[100%]">
+        <div className="h-[100%] lg:h-[90%] ui container" id="home">
+            {/* inclusion of the nav component */}
+            <Nav />
+            
             {/* The component that holds the image and text containers*/}
             <IntroContentContainer />
         </div>
