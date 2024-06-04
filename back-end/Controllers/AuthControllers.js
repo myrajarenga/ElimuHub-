263d81e1b5956dfadca5c9251bfa3c46dc8e776a
atonya-bravin <bravinatonya.brani@gmail.com>
2024-06-05 01:34:39 +0300

    Initial implementation of the controllers

---
+++ back-end/Controllers/AuthControllers.js
@@ -0,0 +1,11 @@
+/*
+    This is the module that contains all the controllers related to Authentications
+*/
+
+export const signIn = (req, res) => {
+    res.send('Sign-In page')
+}
+
+export const signUp = (req, res) => {
+    res.send('Sign-Up page')
+}
